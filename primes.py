f3fc13d4aaa10ede5010ee9d6294a2c451ff19d2
RominaTodor <146735764+RominaTodor@users.noreply.github.com>
2023-12-07 00:49:58 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes<=0:
+        raise ValueError("`primes` was called with argument 0 or a negative number!!!")
+    list=[]
+    num=2
+
+    while len(list)<number_of_primes:
+        prime=True
+        for prime_num in range(2, int(num**0.5)+1):
+            if num%prime_num==0:
+                    prime=False
+                    break
+        if prime:
+            list.append(num)
+        num+=1
     return list
